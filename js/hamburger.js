2c3685f142fb6bf1d3dcef6bca47e1d4efeb5ea2
mitsuhiro.kobayashi <zukunftxiaolin@gmail.com>
2022-09-26 18:46:45 +0900

    hamburuger→×に変化完成

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,10 @@
-const ham = document.querySelector('#js-hamburger'); 
-//js-hamburgerの要素を取得し、変数hamに格納
-const nav = document.querySelector('#js-nav'); 
-//js-navの要素を取得し、変数navに格納
-
-ham.addEventListener('click', function () { 
-    //ハンバーガーメニューをクリックしたら
-    ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
-    nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
-
-});
+// hamburgerという関数を宣言
+function hamburger() {
+    document.getElementById('line1').classList.toggle('line_1');
+    document.getElementById('line2').classList.toggle('line_2');
+    document.getElementById('line3').classList.toggle('line_3');
+    document.getElementById('js-nav').classList.toggle('in');
+}
+document.getElementById('js-hamburger').addEventListener('click' , function () {
+    hamburger();
+} );
